461973f5b85e8008b3278a5a40245e6148586108
qismsom <15191465354@163.com>
2018-06-27 16:26:16 +0800

    第一次提交;

--- module/erp_service/src/components/HelloWorld.vue
+++ module/erp_service/src/components/HelloWorld.vue
@@ -1,105 +1,71 @@
 <template>
-  <div style="padding: 20px">
-    <Card :bordered="false">
-      <p slot="title">员工列表查询</p>
-      <Form :model="formItem" :label-width="120">
-        <Row>
-          <Col span="5" style="margin-left: -20px;">
-          <FormItem label="按单位查询">
-            <Select v-model="formItem.select" style="width: 180px;">
-              <Option value="beijing">New York</Option>
-              <Option value="shanghai">London</Option>
-              <Option value="shenzhen">Sydney</Option>
-            </Select>
-          </FormItem>
-          </Col>
-          <Col span="4" style="margin-left: -30px">
-          <FormItem label="按岗位查询">
-            <Select v-model="formItem.select1" style="width: 180px;">
-              <Option value="beijing">New York</Option>
-              <Option value="shanghai">London</Option>
-              <Option value="shenzhen">Sydney</Option>
-            </Select>
-          </FormItem>
-          </Col>
-          <Col span="15" style="margin-left:40px">
-          <Button type="primary" size="large" style="float: right" @click="exportData(1)"><Icon type="android-download"></Icon>&nbsp;导出Excel</Button>
-          </Col>
-        </Row>
-        <FormItem label="按姓名首字母查询" style="margin-left: -20px">
-          <input v-model="formItem.input1"   type="text" @focus="modal1 = true" placeholder="按照姓名首字母查询" id="select_name">
-        </FormItem>
-        <Modal
-          v-model="modal1"
-          width="300"
-          @on-ok="ok"
-          :mask-closable="false"
-          title="选择姓名首字母" style="text-align: center">
-          <Button type="primary" size="small" :key="index" v-for="(item,index) in letterArray"  id="letter" @click="getLetter(item)">{{item}}</Button>
-        </Modal>
-      </Form>
-    </Card>
+  <div style="padding: 20px; height: 100%">
+    <sxcxArea/>
     <Tabs type="card" style="margin-top: 10px;">
       <Modal
         v-model="modal2"
         width="550"
-        @on-ok="ok"
         :mask-closable="false"
         title="选择要显示的列字段" >
         <Checkbox-group v-model="tableColumnsChecked" @on-change="changeTableColumns">
-          <Checkbox style="width: 150px;margin-left: 12px;" label="show">Show</Checkbox>
-          <Checkbox style="width: 150px;margin-left: 12px;" label="weak">Weak</Checkbox>
-          <Checkbox style="width: 150px;margin-left: 12px;" label="signin">Signin</Checkbox>
-          <Checkbox style="width: 150px;margin-left: 12px;" label="click">Click</Checkbox>
-          <Checkbox style="width: 150px;margin-left: 12px;" label="active">Active</Checkbox>
-          <Checkbox style="width: 150px;margin-left: 12px;" label="day7">7, retained</Checkbox>
-          <Checkbox style="width: 150px;margin-left: 12px;" label="day30">30, retained</Checkbox>
-          <Checkbox style="width: 150px;margin-left: 12px;" label="tomorrow">The next day left</Checkbox>
-          <Checkbox style="width: 150px;margin-left: 12px;" label="day">Day Active</Checkbox>
-          <Checkbox style="width: 150px;margin-left: 12px;" label="week">Week Active</Checkbox>
-          <Checkbox style="width: 150px;margin-left: 12px;" label="month">Month Active</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="show">曾用名</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="weak">性别</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="signin">民族</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="click">出生年月</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="active">工号</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="day7">统计</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="day30">身份证号</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="tomorrow">保险档号</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="day">公积金档号</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="week">公积金状态</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="month">服兵役</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="month1">工作时间</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="month2">退休时间</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="month3">单位</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="month4">部门</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="month5">学历</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="month6">毕业院校</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="month7">所学专业</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="month8">工种</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="month9">职务</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="month10">工作证号</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="month11">合同自起</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="month12">合同止</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="month13">备注</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="month14">路队</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="month15">工作类型</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="month16">备用字段1</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="month17">备用字段2</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="month18">备用字段3</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="month19">备用字段4</Checkbox>
+          <Checkbox style="width: 150px;margin-left: 12px;" label="month20">备用字段5</Checkbox>
         </Checkbox-group>
       </Modal>
       <TabPane label="集团人员查询">
         <Table :data="tableData2" :columns="tableColumns2" border></Table>
       </TabPane>
       <TabPane label="离职人员查询">标签二的内容</TabPane>
+      <Button type="primary" size="small" slot="extra" style="margin-right: 10px;">导出Excel</Button>
       <Button type="primary" size="small" slot="extra" @click="modal2 = true" >列表字段扩展</Button>
     </Tabs>
   </div>
 </template>
 <script>
+  import sxcxArea from './sxcxArea.vue';
   export default {
+    components: {
+      sxcxArea
+    },
     data () {
       return {
         tableData2: this.mockTableData2(),
         tableColumns2: [],
-        tableColumnsChecked: ['show', 'weak', 'signin', 'click', 'active', 'day7', 'day30', 'tomorrow', 'day', 'week', 'month'],
-        letterArray:['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','清除'],
-        getLetters:[],
-        modal1: false,
+//        tableColumnsChecked: ['曾用名', '性别', '民族', '出生年月', '工号', '统计', '身份证号', '保险档号', '公积金档号', '公积金状态', '服兵役', '工作时间', '退休时间', '单位', '部门', '学历', '毕业院校', '所学专业', '工种', '职务', '工作证号', '合同自起', '合同止','备注', '路队', '工作类型', '备用字段1', '备用字段2', '备用字段3','备用字段4','备用字段5'],
+        tableColumnsChecked: ['show', 'weak', 'signin', 'click', 'active', 'day7', 'day30', 'tomorrow', 'day', 'week', 'month', 'month1', 'month2', 'month3', 'month4', 'month5', 'month6', 'month7', 'month8', 'month9', 'month10', 'month11', 'month12', 'month13', 'month14', 'month15', 'month16', 'month17', 'month18','month19','month20'],
         modal2: false,
-        formItem: {
-          select: '',
-          select1: '',
-          input1:''
-        }
       }
     },
     methods: {
-      //        对话框点击确定时候的回调函数
-      ok:function () {
-      },
-      getLetter:function (item) {
-          if(item==='清除'){
-            this.formItem.input1 ='';
-            this.getLetters = []
-          }else {
-            this.getLetters.push(item);
-            let letter = this.getLetters.toString().replace(/,/g,'');
-            this.formItem.input1 = letter
-          }
-      },
       mockTableData2 () {
         let data = [];
         function getNum() {
@@ -108,7 +74,6 @@
         for (let i = 0; i < 2; i++) {
           data.push({
             name: 'Name ' + (i+1),
-            fav: 0,
             show: getNum(),
             weak: getNum(),
             signin: getNum(),
@@ -119,7 +84,26 @@
             tomorrow: getNum(),
             day: getNum(),
             week: getNum(),
-            month: getNum()
+            month: getNum(),
+            month1: getNum(),
+            month2: getNum(),
+            month3: getNum(),
+            month4: getNum(),
+            month5: getNum(),
+            month6: getNum(),
+            month7: getNum(),
+            month8: getNum(),
+            month9: getNum(),
+            month10: getNum(),
+            month11: getNum(),
+            month12: getNum(),
+            month13: getNum(),
+            month19: getNum(),
+            month14: getNum(),
+            month15: getNum(),
+            month16: getNum(),
+            month17: getNum(),
+            month18: getNum(),
           })
         }
         return data;
@@ -127,98 +111,194 @@
       getTable2Columns () {
         const table2ColumnList = {
           name: {
-            title: 'Name',
+            title: '姓名',
             key: 'name',
             fixed: 'left',
             width: 200,
-            render: (h, params) => {
-              const fav = this.tableData2[params.index].fav;
-              const style = fav === 0 ? {
-                cursor: 'pointer'
-              } : {
-                cursor: 'pointer',
-                color: '#f50'
-              };
-
-              return h('div', [
-                h('Icon', {
-                  style: style,
-                  props: {
-                    type: fav === 0 ? 'ios-star-outline' : 'ios-star'
-                  },
-                  nativeOn: {
-                    click: () => {
-                      this.toggleFav(params.index);
-                    }
-                  }
-                }),
-                h('span', ' ' + params.row.name)
-              ]);
-            }
           },
           show: {
-            title: 'Show',
+            title: '曾用名',
             key: 'show',
             width: 150,
             sortable: true
           },
           weak: {
-            title: 'Weak',
+            title: '性别',
             key: 'weak',
             width: 150,
             sortable: true
           },
           signin: {
-            title: 'Signin',
+            title: '民族',
             key: 'signin',
             width: 150,
             sortable: true
           },
           click: {
-            title: 'Click',
+            title: '出生年月',
             key: 'click',
             width: 150,
             sortable: true
           },
           active: {
-            title: 'Active',
+            title: '工号',
             key: 'active',
             width: 150,
             sortable: true
           },
           day7: {
-            title: '7, retained',
+            title: '统计',
             key: 'day7',
             width: 150,
             sortable: true
           },
           day30: {
-            title: '30, retained',
+            title: '身份证号',
             key: 'day30',
             width: 150,
             sortable: true
           },
           tomorrow: {
-            title: 'The next day left',
+            title: '保险档号',
             key: 'tomorrow',
             width: 150,
             sortable: true
           },
           day: {
-            title: 'Day Active',
+            title: '公积金档号',
             key: 'day',
             width: 150,
             sortable: true
           },
           week: {
-            title: 'Week Active',
+            title: '公积金状态',
             key: 'week',
             width: 150,
             sortable: true
           },
           month: {
-            title: 'Month Active',
+            title: '服兵役',
             key: 'month',
+            width: 150,
+            sortable: true
+          },
+          month1: {
+            title: '工作时间',
+            key: 'month1',
+            width: 150,
+            sortable: true
+          },
+          month2: {
+            title: '退休时间',
+            key: 'month2',
+            width: 150,
+            sortable: true
+          },
+          month3: {
+            title: '单位',
+            key: 'month3',
+            width: 150,
+            sortable: true
+          },
+          month4: {
+            title: '部门',
+            key: 'month4',
+            width: 150,
+            sortable: true
+          },
+          month5: {
+            title: '学历',
+            key: 'month5',
+            width: 150,
+            sortable: true
+          },
+          month6: {
+            title: '毕业院校',
+            key: 'month6',
+            width: 150,
+            sortable: true
+          },
+          month7: {
+            title: '所学专业',
+            key: 'month7',
+            width: 150,
+            sortable: true
+          },
+          month8: {
+            title: '工种',
+            key: 'month8',
+            width: 150,
+            sortable: true
+          },
+          month9: {
+            title: '职务',
+            key: 'month9',
+            width: 150,
+            sortable: true
+          },
+          month10: {
+            title: '合同自起',
+            key: 'month10',
+            width: 150,
+            sortable: true
+          },
+          month11: {
+            title: '合同止',
+            key: 'month11',
+            width: 150,
+            sortable: true
+          },
+          month12: {
+            title: '备注',
+            key: 'month12',
+            width: 150,
+            sortable: true
+          },
+          month13: {
+            title: '路队',
+            key: 'month13',
+            width: 150,
+            sortable: true
+          },
+          month14: {
+            title: '备用字段1',
+            key: 'month14',
+            width: 150,
+            sortable: true
+          },
+          month15: {
+            title: '备用字段2',
+            key: 'month15',
+            width: 150,
+            sortable: true
+          },
+          month16: {
+            title: '备用字段3',
+            key: 'month16',
+            width: 150,
+            sortable: true
+          },
+          month17: {
+            title: '备用字段4',
+            key: 'month17',
+            width: 150,
+            sortable: true
+          },
+          month18: {
+            title: '备用字段5',
+            key: 'month18',
+            width: 150,
+            sortable: true
+          },
+          month19: {
+            title: '工作类型',
+            key: 'month19',
+            width: 150,
+            sortable: true
+          },
+          month20: {
+            title: '备用字段5',
+            key: 'month20',
             width: 150,
             sortable: true
           }
@@ -242,19 +322,3 @@
     }
   }
 </script>
-<style>
-  #select_name{
-    width: 180px;
-    border:1px solid  #dddee1;
-    border-radius: 5px;
-    height: 32px;
-    padding-left: 7px;
-  }
-  #select_name::-webkit-input-placeholder {
-    color:#bbbec4;
-  }
-  #letter{
-    margin-right: 10px;width: 50px;margin-bottom: 10px;
-  }
-
-</style>
--- module/erp_service/src/components/sxcxArea.vue
+++ module/erp_service/src/components/sxcxArea.vue
@@ -10,83 +10,89 @@
     margin-top: 10px;
     margin-left: 5px;
   }
+  .select_name{
+    width: 180px;
+    border:1px solid  #dddee1;
+    border-radius: 5px;
+    height: 32px;
+    padding-left: 7px;
+  }
+  .select_name::-webkit-input-placeholder {
+    color:#bbbec4;
+  }
+  .letter{
+    margin-right: 10px;width: 50px;margin-bottom: 10px;
+  }
 </style>
 
 <template>
   <div >
     <Card style="width:100%">
+      <p slot="title">员工列表查询</p>
+      <Button slot="extra" type="primary"  @click.prevent="extensionOptions">更多筛选</Button>
       <div>
+
+        <!--基础查询-->
         <div>
-          <Row class="sxrowStyle" type="flex" align="middle">
-            <Col span="2">
-            首字母:
-            </Col>
-            <Col span="2" class="sxcolStyle">
-            <Input type="text" v-model="nameSpell" placeholder="请输入首字母..."></Input>
-            </Col>
-            <Col span="2" offset="18">
-              <Button type="primary" @click="extensionOptions">
-                更多筛选
-              </Button>
-            </Col>
-          </Row>
-          <Row class="sxrowStyle" type="flex" align="middle">
-            <Col span="2">
-            岗位查询:
-            </Col>
-            <Col span="8" class="sxcolStyle">
-            <Select v-model="postValue" size="middle" style="width:100px">
-              <Option v-for="item in postList" :value="item" :key="item">{{ item }}</Option>
-            </Select>
-            </Col>
-          </Row>
-          <Row>
-            <Col span="2">
-            单位查询:
-            </Col>
-            <Col span="22" class="sxcolStyle">
-              <CheckboxGroup v-model="fruit">
+          <Form :model="jccxFormItem" :label-width="120">
+            <FormItem label="首字母查询" style="margin-left: -50px;">
+              <input v-model="jccxFormItem.szm_select" type="text" @focus="modal1 = true" placeholder="按照姓名首字母查询" class="select_name">
+            </FormItem>
+            <Modal
+              v-model="modal1"
+              width="300"
+              @on-ok="ok"
+              :mask-closable="false"
+              title="选择姓名首字母" style="text-align: center">
+              <Button type="primary" size="small" :key="index" v-for="(item,index) in letterArray"  id="letter" @click="getLetter(item)">{{item}}</Button>
+            </Modal>
+            <FormItem label="按单位查询" style="margin-left: -50px;">
+              <CheckboxGroup v-model="jccxFormItem.dw_radio">
                 <Checkbox label="维修公司"></Checkbox>
                 <Checkbox label="集团公司"></Checkbox>
                 <Checkbox label="公交一公司"></Checkbox>
                 <Checkbox label="公交二公司"></Checkbox>
                 <Checkbox label="公交三公司"></Checkbox>
-                <Checkbox label="公交死公司"></Checkbox>
+                <Checkbox label="公交四公司"></Checkbox>
                 <Checkbox label="公交五公司"></Checkbox>
                 <Checkbox label="公交六公司"></Checkbox>
                 <Checkbox label="公交七公司"></Checkbox>
                 <Checkbox label="公交八公司"></Checkbox>
               </CheckboxGroup>
-            </Col>
-          </Row>
+            </FormItem>
+            <FormItem label="按岗位查询" style="margin-left: -50px;">
+              <Select v-model="jccxFormItem.gw_select" size="middle" style="width:100px">
+                <Option v-for="item in postList" :value="item" :key="item">{{ item }}</Option>
+              </Select>
+            </FormItem>
+          </Form>
         </div>
+
+        <!--更多筛选-->
         <div v-if="sxExtension" style="margin-top: 20px;">
           <hr style="height:1px;border:none;border-top:1px solid lightgrey;margin-bottom: 10px; margin-top: 10px"/>
-          <Row class="sxrowStyle" type="flex" align="middle">
-            <Col span="2">
-              键入搜索
-            </Col>
-            <Col span="8" class="sxcolStyle">
-              <Input v-model="searchOption" placeholder="请输入搜索内容"></Input>
-            </Col>
-          </Row>
-          <!--合同时间-->
-          <Row class="sxrowStyle" type="flex" align="middle" justify="start">
-            <Col span="2">合同自启</Col>
-            <Col span="3" style="margin-left: -20px">
-              <DatePicker type="date" placeholder="Select date"></DatePicker>
-            </Col>
-            <Col span="1" style="text-align: center">-</Col>
-            <Col span="2" >合同终止</Col>
-            <Col span="3" style="margin-left: -20px">
-            <DatePicker type="date" placeholder=""></DatePicker>
-            </Col>
-          </Row>
+          <Form :model="gesxFormItem" :label-width="120">
+            <FormItem label="键入搜索" style="margin-left: -50px;">
+              <input v-model="searchOption" type="text" placeholder="请输入搜索内容" class="select_name"/>
+            </FormItem>
+            <Row>
+              <Col span="6" >
+                <FormItem label="合同自起" style="margin-left: -50px;">
+                  <DatePicker type="date" placeholder="请选择合同自起时间"></DatePicker>
+                </FormItem>
+              </Col>
+              <Col span="6">
+                <FormItem label="合同终止" style="margin-left: -50px;">
+                  <DatePicker type="date" placeholder="请选择合同终止时间"></DatePicker>
+                </FormItem>
+              </Col>
+            </Row>
+          </Form>
 
           <Tabs type="card">
             <TabPane label="民族">
               <RadioGroup v-model="animal">
-                <Radio size="large" v-for="item in mzList" :key="item">{{item}}</Radio>
+                <Radio size="large" v-for="item in mzList" :key="item">{{item.name}}</Radio>
               </RadioGroup>
             </TabPane>
             <TabPane label="部门">
@@ -123,11 +129,10 @@
                 <Radio class="sxradioStyle" size="large" v-for="item in xlList" :key="item">{{item}}</Radio>
               </RadioGroup>
             </TabPane>
-
           </Tabs>
-
-
         </div>
+
+        <!--筛选结果-->
         <div>
           <hr style="height:1px;border:none;border-top:1px solid lightgrey;margin-bottom: 10px; margin-top: 10px"/>
           <Row type="flex" align="middle" gutter="16">
@@ -163,48 +168,41 @@
         props: {
           row: Object
         },
+        modal1: false,
+        jccxFormItem: {
+          szm_select: '',
+          gw_select: '',
+          dw_radio: '',
+        },
         value: '哈哈',
         nameSpell: '',
-        xueliList: [
-        	'博士',
-          '硕士',
-          '大学',
-          '大专',
-          '中专中技',
-          '技校',
-          '高中',
-          '职高',
-          '初中',
-          '小学',
-          '文盲或半文盲',
-        ],
-        postList: [
-        	'全部',
-        	'公司领导',
-          '二级',
-          '三级',
-          '主任科员',
-          '一般管理',
-          '辅助',
-          '司机',
-          '修理',
-          '其他在岗',
-          '内退',
-          '病假',
-          '产假',
-          '女工长假',
-          '下岗',
-          '工伤',
-          '待岗',
-          '停薪',
-          '外借',
-          '其他不在岗',
-        ],
+        xueliList: ['博士', '硕士', '大学', '大专', '中专中技', '技校', '高中', '职高', '初中', '小学', '文盲或半文盲',],
+        postList: ['全部', '公司领导', '二级', '三级', '主任科员', '一般管理', '辅助', '司机', '修理', '其他在岗', '内退', '病假', '产假', '女工长假','下岗','工伤','待岗', '停薪', '外借','其他不在岗',],
+        getLetters:[],
+        letterArray:['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','清除'],
         searchOption: '',
         bmList: [],
         postValue: '',
         sxExtension: false,
-        mzList: ['汉族', '回族', '满族'],
+        mzList: [{"id":"01","name":"汉族"},{"id":"02","name":"蒙古族"},{"id":"03","name":"回族"},
+          {"id":"04","name":"藏族"},{"id":"05","name":"维吾尔族"},{"id":"06","name":"苗族"},
+          {"id":"07","name":"彝族"},{"id":"08","name":"壮族"},{"id":"09","name":"布依族"},
+          {"id":"10","name":"朝鲜族"},{"id":"11","name":"满族"},{"id":"12","name":"侗族"},
+          {"id":"13","name":"瑶族"},{"id":"14","name":"白族"},{"id":"15","name":"土家族"},
+          {"id":"16","name":"哈尼族"},{"id":"17","name":"哈萨克族"},{"id":"18","name":"傣族"},
+          {"id":"19","name":"黎族"},{"id":"20","name":"傈僳族"},{"id":"21","name":"佤族"},
+          {"id":"22","name":"畲族"},{"id":"23","name":"高山族"},{"id":"24","name":"拉祜族"},
+          {"id":"25","name":"水族"},{"id":"26","name":"东乡族"},{"id":"27","name":"纳西族"},
+          {"id":"28","name":"景颇族"},{"id":"29","name":"柯尔克孜族"},{"id":"30","name":"土族"},
+          {"id":"31","name":"达斡尔族"},{"id":"32","name":"仫佬族"},{"id":"33","name":"羌族"},
+          {"id":"34","name":"布朗族"},{"id":"35","name":"撒拉族"},{"id":"36","name":"毛难族"},
+          {"id":"37","name":"仡佬族"},{"id":"38","name":"锡伯族"},{"id":"39","name":"阿昌族"},
+          {"id":"40","name":"普米族"},{"id":"41","name":"塔吉克族"},{"id":"42","name":"怒族"},
+          {"id":"43","name":"乌孜别克族"},{"id":"44","name":"俄罗斯族"},{"id":"45","name":"鄂温克族"},
+          {"id":"46","name":"崩龙族"},{"id":"47","name":"保安族"},{"id":"48","name":"裕固族"},
+          {"id":"49","name":"京族"},{"id":"50","name":"塔塔尔族"},{"id":"51","name":"独龙族"},
+          {"id":"52","name":"鄂伦春族"},{"id":"53","name":"赫哲族"},{"id":"54","name":"门巴族"},
+          {"id":"55","name":"珞巴族"},{"id":"56","name":"基诺族"}],
         gzlxList: ['实习', '试用'],
         bmList: ['运营部', '企管部', '技术部', '劳动人事部', '安全部', '燃油供应中心', '信息中心', '运营检验处（技检处）', '车辆统计', '经营管理处', '保养车间', '钢瓶检测', '技术处', '技术处'],
         gzzwList: ['司机','站员', '清洁', '经警', '纪检委副书记', '监检室主任'],
@@ -214,7 +212,17 @@
     methods: {
       extensionOptions() {
         this.sxExtension = (this.sxExtension === false) ? true : false;
-      }
+      },
+      getLetter:function (item) {
+        if(item==='清除'){
+          this.jccxFormItem.szm_select ='';
+          this.getLetters = []
+        }else {
+          this.getLetters.push(item);
+          let letter = this.getLetters.toString().replace(/,/g,'');
+          this.jccxFormItem.szm_select = letter;
+        }
+      },
     }
   }
 </script>
